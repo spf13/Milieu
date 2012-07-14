c99261d934c04092de52a572714049316013972b
Steve Francia <steve.francia@gmail.com>
2012-07-14 02:00:31 -0400

    Take 3

--- model/mongodb.rb
+++ model/mongodb.rb
@@ -5,12 +5,10 @@ require './model/user'
 if ENV['RACK_ENV'] == 'production'
   db = URI.parse(ENV['MONGOHQ_URL'])
   db_name = db.path.gsub(/^\//, '')
-  CONNECTION = Mongo::Connection.new(db.host, db.port).db(db_name)
-  CONNECTION.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
-  DB = CONNECTION.db(db_name)
+  DB = Mongo::Connection.new(db.host, db.port).db(db_name)
+  DB.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
 else
-  CONNECTION = Mongo::Connection.new("localhost", 27017)
-  DB         = CONNECTION.db('milieu')
+  DB = Mongo::Connection.new("localhost", 27017).db('milieu')
 end
 
 USERS      = DB['users']
