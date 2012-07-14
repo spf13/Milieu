ad894be01fcbfa1c0d355455256ca390c338318b
Steve Francia <steve.francia@gmail.com>
2012-07-13 23:46:35 -0400

    adding quotes

--- model/mongodb.rb
+++ model/mongodb.rb
@@ -2,10 +2,10 @@ require 'mongo'
 require './model/mongoModule'
 require './model/user'
 
-if ENV['RACK_ENV'] == production
-    CONNECTION = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+if ENV['RACK_ENV'] == 'production'
+  CONNECTION = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
 else
-    CONNECTION = Mongo::Connection.new("localhost", 27017)
+  CONNECTION = Mongo::Connection.new("localhost", 27017)
 end
 
 DB         = CONNECTION.db('milieu')
