9d5d68844f9e082c88d52f302e452102d505c674
Steve Francia <steve.francia@gmail.com>
2012-07-13 23:41:12 -0400

    Adding in production MongoDB connection

--- model/mongodb.rb
+++ model/mongodb.rb
@@ -2,7 +2,12 @@ require 'mongo'
 require './model/mongoModule'
 require './model/user'
 
-CONNECTION = Mongo::Connection.new("localhost", 27017)
+if ENV['RACK_ENV'] == production
+    CONNECTION = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+else
+    CONNECTION = Mongo::Connection.new("localhost", 27017)
+end
+
 DB         = CONNECTION.db('milieu')
 USERS      = DB['users']
 VENUES     = DB['venues']
