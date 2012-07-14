64d771462b0cb9d7ff4d9c539b968e019505d571
Steve Francia <steve.francia@gmail.com>
2012-07-14 11:48:37 -0400

    Adding comments and stripping out unused routes

--- app.rb
+++ app.rb
@@ -11,6 +11,8 @@ configure do
   enable :sessions
 end
 
+# This runs prior to all requests.
+# It passes along the logged in user's object (from the session)
 before do
   unless session[:user] == nil
     @suser = session[:user]
@@ -21,18 +23,12 @@ get '/' do
   haml :index
 end
 
-get '/user' do
-  redirect '/user/' + session[:user].email + '/profile'
-end
-
-get '/about' do
-  haml :about
-end
-
 get '/login' do
   haml :login
 end
 
+# The login post routine will take the provided params and run the auth routine.
+# If the auth routine is successful it will return the user object, else nil
 post '/login' do
   if session[:user] = User.auth(params["email"], params["password"])
     flash("Login successful")
@@ -54,17 +50,21 @@ get '/register' do
 end
 
 post '/register' do
+  # Creating and populating a new user object from the DB
   u            = User.new
   u.email      = params[:email]
   u.password   = params[:password]
   u.name       = params[:name]
-  #u.email_hash = Digest::MD5.hexdigest(params[:email].downcase)
 
+  # Attempt to save the user to the DB
   if u.save()
     flash("User created")
+
+    # If user saved, authenticate from the database
     session[:user] = User.auth( params["email"], params["password"])
     redirect '/user/' << session[:user].email.to_s << "/dashboard"
   else
+    # Else, display errors
     tmp = []
     u.errors.each do |e|
       tmp << (e.join("<br/>"))
@@ -78,6 +78,14 @@ get '/user/:email/dashboard' do
   haml :user_dashboard
 end
 
+get '/user' do
+  if logged_in?
+      redirect '/user/' + session[:user].email + '/profile'
+  else
+      redirect '/'
+  end
+end
+
 get '/user/:email/profile' do
   @user = User.new_from_email(params[:email])
   haml :user_profile
@@ -88,6 +96,9 @@ get '/list' do
   haml :list
 end
 
+# Listing the venues.
+# As a warning, this approach to pagination isn't especially effective
+# when dealing with large result sets.
 get '/venues/?:page?' do
   @page = params.fetch('page', 1).to_i
   num_per_page = 10
@@ -97,33 +108,44 @@ get '/venues/?:page?' do
 end
 
 get '/venue/:_id' do
+  # Converting the string _id from the url into an ObjectId
   object_id = BSON::ObjectId.from_string(params[:_id])
+
+  # Query for the venue from MongoDB based on the ObjectId
   @venue = VENUES.find_one({ :_id => object_id })
+
+  # Grab the session user from the db to accurately provide stats
   @user = User.new_from_email(@suser.email) if logged_in?
+
+  # Find 4 closest venues to this one
   @nearby_venues = VENUES.find(
-      { :'location.geo' => 
+      { :'location.geo' =>
           { :$near => [ @venue['location']['geo'][0],
               @venue['location']['geo'][1]]
           }
       }).limit(4).skip(1)
+
+  # Render the template
   haml :venue
 end
 
-# Add lots of comments here
 get '/venue/:_id/checkin' do
+  # Converting the string _id from the url into an ObjectId
   object_id = BSON::ObjectId.from_string(params[:_id])
+
+  # Query for the venue from MongoDB based on the ObjectId
   @venue = VENUES.find_one({ :_id => object_id })
+
+  # Simultaneously add the users checkin to the venue & return it.
   user = USERS.find_and_modify(:query => { :_id => @suser._id}, :update => {:$inc => { "venues." << object_id.to_s => 1 } }, :new => 1)
+
+  # If it's the first time, increment both checkins and users counts
   if user['venues'][params[:_id]] == 1
       VENUES.update({ :_id => @venue['_id']}, { :$inc => { :'stats.usersCount' => 1, :'stats.checkinsCount' => 1}})
+  # Else, just the increment the checkins
   else
       VENUES.update({ _id: @venue['_id']}, { :$inc => { :'stats.checkinsCount' => 1}})
   end
   flash('Thanks for checking in')
   redirect '/venue/' + params[:_id]
-end
-
-get '/checkin/:location' do
-   @venues = VENUES.find( )
-   haml :venues
 end
