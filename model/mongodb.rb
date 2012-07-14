6f13066ff58f520ac1e51adff6175e4cbdf15223
Steve Francia <steve.francia@gmail.com>
2012-07-14 01:53:28 -0400

    More DB settings to work with Heroku

--- model/mongodb.rb
+++ model/mongodb.rb
@@ -3,13 +3,15 @@ require './model/mongoModule'
 require './model/user'
 
 if ENV['RACK_ENV'] == 'production'
-  CONNECTION = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+  db = URI.parse(ENV['MONGOHQ_URL'])
+  DB = db.path.gsub(/^\//, '')
+  CONNECTION = Mongo::Connection.new(db.host, db.port).db(db_name)
+  CONNECTION.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
 else
   CONNECTION = Mongo::Connection.new("localhost", 27017)
+  DB         = CONNECTION.db('milieu')
 end
 
-DB         = CONNECTION.db('milieu')
 USERS      = DB['users']
 VENUES     = DB['venues']
 CHECKINS   = DB['checkins']
-
